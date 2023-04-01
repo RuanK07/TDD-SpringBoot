ba467adccadf9dd8b9e27419d2d970f09960b1ed
Ruan Kennedy <Ruank07@gmail.com>
2023-03-31 23:37:06 -0300

    Salvando nova pessoa

--- src/main/java/com/exemplo/demo/resource/PessoaResource.java
+++ src/main/java/com/exemplo/demo/resource/PessoaResource.java
@@ -1,17 +1,26 @@
 package com.exemplo.demo.resource;
 
+import java.net.URI;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.exemplo.demo.modelo.Pessoa;
 import com.exemplo.demo.modelo.Telefone;
 import com.exemplo.demo.servico.PessoaService;
 import com.exemplo.demo.servico.exception.TelefoneNaoEncontradoException;
+import com.exemplo.demo.servico.exception.UnicidadeCpfException;
+import com.exemplo.demo.servico.exception.UnicidadeTelefoneException;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -32,5 +41,16 @@ public class PessoaResource {
 		
 		return new ResponseEntity<>(pessoa, HttpStatus.OK);
 		}
+	
+	@PostMapping
+	public ResponseEntity<Pessoa> salvarNova(@RequestBody Pessoa pessoa, HttpServletResponse response) throws UnicidadeCpfException, UnicidadeTelefoneException {
+		final Pessoa pessoaSalva = pessoaService.salvar(pessoa);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{ddd}/{numero}")
+				.buildAndExpand(pessoa.getTelefones().get(0).getDdd(), pessoa.getTelefones().get(0).getNumero()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return new ResponseEntity<>(pessoaSalva, HttpStatus.CREATED);
+	}
 	
 }
--- src/test/java/com/exemplo/demo/resources/PessoaResourceTest.java
+++ src/test/java/com/exemplo/demo/resources/PessoaResourceTest.java
@@ -2,17 +2,22 @@ package com.exemplo.demo.resources;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
 import static io.restassured.RestAssured.given;
 
 import com.example.demo.DemoApplicationTests;
+import com.exemplo.demo.modelo.Pessoa;
+import com.exemplo.demo.modelo.Telefone;
+
+import io.restassured.http.ContentType;
 
 public class PessoaResourceTest extends DemoApplicationTests{
 	
 	@Test
-	public void deve_procurar_pessoa_pelo_ddd_e_numero_do_telefone() throws Exception {
+	void deve_procurar_pessoa_pelo_ddd_e_numero_do_telefone() throws Exception {
 		given()
 				.pathParam("ddd", "86")
 				.pathParam("numero", "35006330")
@@ -27,7 +32,7 @@ public class PessoaResourceTest extends DemoApplicationTests{
 	}
 	
 	@Test
-	public void deve_retornar_erro_nao_encontrado_quando_buscar_pessoa_por_telefone_inexistente() throws Exception {
+	void deve_retornar_erro_nao_encontrado_quando_buscar_pessoa_por_telefone_inexistente() throws Exception {
 		given()
 				.pathParam("ddd", "99")
 				.pathParam("numero", "987654321")
@@ -36,5 +41,34 @@ public class PessoaResourceTest extends DemoApplicationTests{
 				.log().body().and()
 				.statusCode(HttpStatus.NOT_FOUND.value())
 				.body("erro", equalTo("Não existe pessoa com o telefone (99)987654321"));
+	}
+	
+	@Test
+	void deve_salvar_nova_pessoa_no_sistema() throws Exception {
+		final Pessoa pessoa = new Pessoa();
+		pessoa.setNome("Lorenzo");
+		pessoa.setCpf("62461410720");
+		
+		final Telefone telefone = new Telefone();
+		telefone.setDdd("79");
+		telefone.setNumero("36977168");
+		
+		pessoa.adicionaTelefone(telefone);
+		
+		given()
+				.request()
+				.header("Accept", ContentType.ANY)
+				.header("Content-type", ContentType.JSON)
+				.body(pessoa)
+		.when()
+		.post("/pessoas")
+		.then()
+				.log().headers().and()
+				.log().body().and()
+				.statusCode(HttpStatus.CREATED.value())
+				.header("Location", equalTo("http://localhost:" + porta + "/pessoas/67/37132848"))
+				.body("codigo", equalTo(6),
+					  "nome", equalTo("Fernanda"),
+					  "cpf", equalTo("27161503469"));
 	}
 }
